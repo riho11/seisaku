a18602f48990f1ff5d77ffea8ee5895f78fd66a7
riho11 <pinooniq0301@gmail.com>
2021-10-24 21:33:28 +0900

    開発中画面

---
+++ q&a.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>食事</title>
+</head>
+<body>
+    <p>開発中</p>
+</body>
+</html>
---
+++ sports.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>運動</title>
+</head>
+<body>
+    <p>開発中</p>
+</body>
+</html>
